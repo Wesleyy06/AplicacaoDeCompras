ce1e59c79fcf3ddf98c066e715549e83ff588acb
Wesleyy06 <wesleydesousa34@gmail.com>
2025-03-06 14:40:28 -0300

    Implementação da classe Product

---
+++ src/models/Product.java
@@ -0,0 +1,20 @@
+package models;
+
+public class Product {
+    String name;
+    double value;
+
+    public Product(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
+
