a075ad06b37009fca9cf665c249a17fce1476983
Wesleyy06 <wesleydesousa34@gmail.com>
2025-03-07 12:23:00 -0300

    Atualização no metodo payment implementando funções de troco e verificação de dinheiro disponivel para transação.

--- src/models/CashPayment.java
+++ src/models/CashPayment.java
@@ -2,6 +2,7 @@ package models;
 
 public class CashPayment extends User implements Payment{
     private double moneyInWalllet;
+    private double paymentAmount;
 
     public CashPayment(String name, String cpf) {
         super(name, cpf);
@@ -11,10 +12,27 @@ public class CashPayment extends User implements Payment{
         return moneyInWalllet;
     }
 
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(double paymentAmount) {
+        if(paymentAmount < moneyInWalllet) {
+            System.out.println("Você não possui dinheiro suficiente para realizar esta transação!");
+        }
+        else {
+            this.paymentAmount = paymentAmount;
+        }
+
+    }
+
     @Override
     public void payMent(Product amount) {
         if(moneyInWalllet < amount.getValue()) {
             System.out.println("Saldo insuficiente na carteira para efeutar a compra.");
+        }
+        if (paymentAmount < amount.getValue()) {
+            double change = this.paymentAmount -= amount.getValue();
         }
         this.moneyInWalllet -= amount.getValue();
     }
